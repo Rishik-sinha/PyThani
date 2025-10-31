fb28c7af12551d8561077026c99bdf0822da4f2d
Rishik Sinha <kumarrishiksinha620@gmail.com>
2025-10-31 16:13:16 +0530

    Add deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // --- ADD THIS LINE ---
+  base: "/PyThani/", // This must match your repository name
+
   plugins: [react()],
   server: {
     proxy: {
-      // This will proxy any request starting with /api
       '/api': {
-        target: 'https://api.freepik.com', // The real API server
-        changeOrigin: true, // This is required for it to work
-        secure: false, // You can set this to false for http/https mix
-        rewrite: (path) => path.replace(/^\/api/, ''), // Removes /api from the path
+        target: 'https://api.freepik.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
